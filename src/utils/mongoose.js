7e00d6499aea083bfd507cc7f6085d12ac05eb21
marcosfacchetti <marcos-facchetti@hotmail.com>
2024-01-08 02:15:11 -0300

    merindomasss

--- src/utils/mongoose.js
+++ src/utils/mongoose.js
@@ -1,6 +1,31 @@
-import { connectDB } from "./dbConnect";
+import { connect, connection } from "mongoose";
 
-// ... tu código existente para la aplicación
+const conn = {
+    isConnected: false
+}
 
-// Llamada a la función de conexión después de la construcción estática
-export default connectDB();
+export async function connectDB() {
+    if (conn.isConnected) return;
+
+    const options = {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        // Establecer tiempos de espera en milisegundos
+        // Puedes ajustar estos valores según tus necesidades y la capacidad de respuesta de tu base de datos
+        socketTimeoutMS: 30000, // Tiempo de espera para operaciones de red
+        connectTimeoutMS: 30000 // Tiempo de espera para la conexión inicial
+    };
+
+    // Conectar con las opciones configuradas
+    const db = await connect("mongodb+srv://marcosfacchetti9n:pIP2mInOncpEleS1@cluster1.ta4frcx.mongodb.net/?retryWrites=true&w=majority", options);
+
+    conn.isConnected = db.connections[0].readyState;
+}
+
+connection.on("connected", () => {
+    console.log("Mongoose is connected");
+});
+
+connection.on("error", (err) => {
+    console.log("Mongoose connection error", err);
+});
