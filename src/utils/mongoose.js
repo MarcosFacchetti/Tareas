bf59fe95464d15d34a92fa8a87075bf317691f7d
marcosfacchetti <marcos-facchetti@hotmail.com>
2024-01-08 02:08:33 -0300

    merindomas

--- src/utils/mongoose.js
+++ src/utils/mongoose.js
@@ -3,4 +3,4 @@ import { connectDB } from "./dbConnect";
 // ... tu código existente para la aplicación
 
 // Llamada a la función de conexión después de la construcción estática
-connectDB();
+export default connectDB();
